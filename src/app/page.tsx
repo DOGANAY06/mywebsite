d6eee5e962a092e42803f2a5335988ec7cdb97e2
Doğan AY <dogan.ay@cbfo.gov.tr>
2024-05-19 13:30:12 +0300

    Public kisimlari commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,246 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <div className="flex bg-gray-500">
+      <div className="w-2/5 top-0 box-border p-5 h-screen">
+        <div className="pt-16 pb-16">
+          <img
+            className="w-1/3 rounded-full h-50 object-cover mb-10"
+            src="/dogan.jpeg"
+            alt=""
+          />
+          <div className="font-semibold text-3xl mb-10">Doğan AY</div>
+          <div className="flex font-medium">
+            Teknolojiye olan tutkum, lisede okumaya başladığımda filizlendi. Bu
+            tutkum beni bilgisayar mühendisliği okumaya teşvik etti ve bölümümü
+            2.likle tamamladım. Eğitimimi alırken edindiğim bilgi ve becerileri
+            kullanarak, kurumsal firmalara web uygulamaları geliştirme konusunda
+            full-stack rolünde hizmet veriyorum. Benim için sadece kod yazmak
+            önemli değil, aynı zamanda insanlarla etkileşim kurmak ve onların
+            ihtiyaçlarını anlamak önemlidir. Müşterilerimle yakın ilişkiler
+            kurmaya ve onların hedeflerine ulaşmalarına yardımcı olmaya özen
+            gösteriyorum.
+          </div>
+        </div>
+        <div className="flex">
+          <a href="mailto:doganay58068@gmail.com" target="_blank">
+            <div className="w-auto h-auto bg-amber-200 flex items-center pt-1 pb-1 pl-2 pr-2 rounded-md mr-5">
+              <img
+                className="w-5"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png"
+                alt="Gmail"
+              />
+              <div className="pl-3 text-sm text-black">Gmail</div>
+            </div>
+          </a>
+
+          <a href="./DoganAY_cv.pdf" download>
+            <div className="w-auto h-auto bg-red-200 flex items-center pt-1 pb-1 pl-2 pr-2 rounded-md">
+              <img
+                className="w-5"
+                src="https://seeklogo.com/images/A/acrobat-file-pdf-logo-37A1BFDE35-seeklogo.com.png"
+                alt="PDF"
+              />
+              <div className="pl-3 text-sm text-black">Özgeçmiş CV</div>
+            </div>
           </a>
         </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-3">Çalıştığım Alanlar</h2>
+          <ul className="list-disc pl-5 text-gray-700">
+            <li>Full Stack Web Geliştirme</li>
+            <li>WordPress Web Geliştirme</li>
+            <li>Teknik Destek</li>
+            <li>Web Tasarımı</li>
+          </ul>
+        </div>
+        <a className="p-5" href="https://wa.me/905071235726" target="_blank">
+        <div className="w-auto h-auto bg-green-200 flex items-center pt-1 pb-1 pl-2 pr-2 rounded-md ml-5">
+          <img
+            className="w-10"
+            src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+            alt="WhatsApp"
+          />
+          <div className="pl-3 text-sm text-black">
+          <span className="text-xl font-semibold mb-3 text-black">İş Teklifi ve Önerileriniz için iletişime geçebilirsiniz.</span>
+          </div>
+        </div>
+      </a>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="">
+        <div className="container flex flex-wrap justify-between">
+          <a target="_blank" href="https://www.instagram.com/_dogan_ay_06/">
+            <div className="w-32 border-2 h-32 p-3 rounded-lg bg-rose-300 mr-10">
+              <img
+                className=" w-8 pb-3"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png"
+                alt=""
+              />
+              <div className="text-xs text-white pb-3">@_dogan_ay_06</div>
+              <div className="bg-rose-500 p-1 text-white text-xs rounded-lg w-fit font-medium">
+                Takip Et
+              </div>
+            </div>
+          </a>
+          <a target="_blank" href="https://github.com/DOGANAY06">
+            <div className="w-32 border-2 h-32 p-3 rounded-lg bg-white mr-10">
+              <img
+                className=" w-8 pb-3"
+                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                alt=""
+              />
+              <div className="text-xs text-black pb-3">doganay06</div>
+              <div className="bg-black p-1 text-white text-xs rounded-lg w-fit font-medium">
+                Takip Et
+              </div>
+            </div>
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/do%C4%9Fanay/">
+            <div className="w-32 border-2 h-32 p-3 rounded-lg bg-blue-600 mr-10">
+              <img
+                className=" w-8 pb-3"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png"
+                alt=""
+              />
+              <div className="text-xs text-white pb-3">@doğanay</div>
+              <div className="bg-sky-500 p-1 text-white text-xs rounded-lg w-fit font-medium">
+                Bağlantı Kur
+              </div>
+            </div>
+          </a>
+          <a target="_blank" href="https://medium.com/@dogan-ay">
+            <div className="w-32 border-2 h-32 p-3 rounded-lg bg-white mr-10">
+              <img
+                className=" w-8 pb-3"
+                src="https://cdn4.iconfinder.com/data/icons/social-media-circle-7/512/Medium_circle-512.png"
+                alt=""
+              />
+              <div className="text-xs text-black pb-3">@dogan-ay</div>
+              <div className="bg-black p-1 text-white text-xs rounded-lg w-fit font-medium">
+                Follow
+              </div>
+            </div>
+          </a>
+          <div className="lg:flex lg:flex-wrap p-5">
+            <a className="p-5" href="https://www.ifm.gov.tr/" target="_blank">
+              <div className="w-96 h-auto bg-gray-600 rounded-xl p-3">
+                <img
+                  className="w-10 pb-2"
+                  src="https://www.ifm.gov.tr/dist/media/img/logo-en_mobile.svg"
+                  alt=""
+                />
+                <div className="pb-2 text-xs-bold font-medium text-white">
+                  İstanbul Finans Merkezi Projesi
+                </div>
+                <div className="font-medium text-xs text-white pb-2">
+                  Full Stack Developer olarak geliştirmeler yapmaktayım.
+                </div>
+                <img
+                  className="rounded-xl"
+                  src="https://www.ifm.gov.tr/dist/media/img/home/ifmvideo2023.png"
+                  alt=""
+                />
+              </div>
+            </a>
+            <a
+              className="p-5"
+              href="https://ankaramamakspot.com/"
+              target="_blank"
+            >
+              <div className="w-96 h-auto bg-gray-600 rounded-xl p-3">
+                <img
+                  className="w-10 pb-2"
+                  src="https://ankaramamakikincielesya.com/wp-content/uploads/2023/02/logo-1-164x84.png"
+                  alt=""
+                />
+                <div className="pb-2 text-xs font-medium text-white">
+                  Furkan Spot Ankara Mamak İkinci El Eşya Alım & Satım
+                </div>
+                <div className="font-medium text-xs text-white pb-2">
+                  Freelancer Olarak Geliştirdiğim Projem
+                </div>
+                <img
+                  className="rounded-xl"
+                  src="https://ankaramamakikincielesya.com/wp-content/uploads/2023/02/logo-1.png"
+                  alt=""
+                />
+              </div>
+            </a>
+            <a
+              className="p-5"
+              href="https://play.google.com/store/books/details/Do%C4%9Fan_AY_C_Programlama_%C3%96rnekleri?id=q1rrDwAAQBAJ"
+              target="_blank"
+            >
+              <div className="w-96 h-auto bg-gray-600 rounded-xl p-3">
+                <img
+                  className="w-10 pb-2"
+                  src="https://books.google.com/books/publisher/content/images/frontcover/q1rrDwAAQBAJ?fife=w240-h345"
+                  alt=""
+                />
+                <div className="pb-2 text-xs font-medium text-white">
+                C Programlama & Örnekleri
+                </div>
+                <div className="font-medium text-xs text-white pb-2">
+                  Bilgisayar Mühendisliği 1.sınıf Programlama dersi konularını içerir.
+                </div>
+                <img
+                  className="rounded-xl"
+                  src="https://books.google.com/books/publisher/content/images/frontcover/q1rrDwAAQBAJ?fife=w240-h345"
+                  alt=""
+                />
+              </div>
+            </a>
+            <a
+              className="p-5"
+              href="https://play.google.com/store/books/details/Do%C4%9Fan_AY_Mant%C4%B1k_Devreleri?id=2BJEEAAAQBAJ"
+              target="_blank"
+            >
+              <div className="w-96 h-auto bg-gray-600 rounded-xl p-3">
+                <img
+                  className="w-10 pb-2"
+                  src="https://books.google.com/books/publisher/content/images/frontcover/2BJEEAAAQBAJ?fife=w240-h345"
+                  alt=""
+                />
+                <div className="pb-2 text-xs font-medium text-white">
+                  Mantık Devreleri: Sayısal Elektronik
+                </div>
+                <div className="font-medium text-xs text-white pb-2">
+                Bilgisayar Mühendisliği 2.sınıf Mantık Devreleri dersi konularını içerir.
+                </div>
+                <img
+                  className="rounded-xl"
+                  src="https://books.google.com/books/publisher/content/images/frontcover/2BJEEAAAQBAJ?fife=w240-h345"
+                  alt=""
+                />
+              </div>
+            </a>
+            <a
+              className="p-5"
+              href="https://ankaramamakspot.com/"
+              target="_blank"
+            >
+              <div className="w-96 h-auto bg-gray-600 rounded-xl p-3">
+                <img
+                  className="w-10 pb-2"
+                  src="https://ankaramamakspot.com/wp-content/uploads/2022/03/cropped-aaQV8rg.png"
+                  alt=""
+                />
+                <div className="pb-2 text-xs font-medium text-white">
+                  Ankara Mamak Spot
+                </div>
+                <div className="font-medium text-xs text-white pb-2">
+                  Freelancer Olarak Geliştirdiğim Projem
+                </div>
+                <img
+                  className="rounded-xl"
+                  src="https://ankaramamakspot.com/wp-content/gallery/girissayfasigalery/3742dcff-ef18-4721-ae53-4248be2f4889.jpg"
+                  alt=""
+                />
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
